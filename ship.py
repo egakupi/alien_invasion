43b83831b9a440b5cadad405c202c1ccadb72ca6
Egakupi <dmisavchenkv@gmail.com>
2018-09-17 20:50:39 +0300

    Update with left and right move and walls

--- ship.py
+++ ship.py
@@ -3,8 +3,9 @@ import pygame
 
 class Ship:
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         self.screen = screen
+        self.ai_settings = ai_settings
 
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
@@ -12,15 +13,18 @@ class Ship:
         # Each new ship appears at the bottom of the screen
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
-        # moving flag
+        # Save coord of ship center
+        self.center = float(self.rect.centerx)
+        # Moving flag
         self.moving_left = False
         self.moving_right = False
 
     def update(self):
-        if self.moving_left:
-            self.rect.centerx -= 1
-        if self.moving_right:
-            self.rect.centerx += 1
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Print ship in current position"""
