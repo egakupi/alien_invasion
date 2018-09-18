53e23bc94327e9bf05d57e4cbe3550ef3823f4c6
Egakupi <dmisavchenkv@gmail.com>
2018-09-18 23:47:58 +0300

    Added first row of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,7 +4,6 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
-from alien import Alien
 import game_functions as gf
 
 
@@ -17,13 +16,14 @@ def run_game():
 
     ship = Ship(ai_settings, screen)
     bullets = Group()
-    alien = Alien(ai_settings, screen)
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, aliens)
 
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
