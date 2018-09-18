d25c72932f5c245206354d2252aafb877cf5ac7c
Egakupi <dmisavchenkv@gmail.com>
2018-09-18 23:35:50 +0300

    Alien on screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -17,13 +17,13 @@ def run_game():
 
     ship = Ship(ai_settings, screen)
     bullets = Group()
-    alien = Alien(screen)
+    alien = Alien(ai_settings, screen)
 
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 
 run_game()
